8719fd1590f457ee8b740927ea86e4b844870803
m.leona <m.leona@ISIS2603S1-0075.sis.virtual.uniandes.edu.co>
2018-02-11 17:53:44 -0500

    Added FacturaDTO, FacturaDetailDTO, TarjetaPuntosDTO and TarjetaPuntosDetailDTO

---
+++ pasteleando-web/src/main/java/co/edu/uniandes/csw/pasteleando/dtos/TarjetaPuntosDetailDTO.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.pasteleando.dtos;
+
+/**
+ *
+ * @author m.leona
+ */
+public class TarjetaPuntosDetailDTO extends TarjetaPuntosDTO{
+    
+    /*
+    constructor por defecto
+    */
+    public TarjetaPuntosDetailDTO(int pNumeroPuntos, boolean pEsRedimible)
+    {
+        super(pNumeroPuntos, pEsRedimible);
+    }
+}
