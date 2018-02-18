27e4d3b23334c11e8919438525293fbc048d3649
paolabay <mp.bayonal@uniandes.edu.co>
2018-02-18 18:18:46 -0500

    Cliente persistance

--- pasteleando-back/src/main/java/co/edu/uniandes/csw/pasteleando/persistence/ClientePersistance.java
+++ pasteleando-back/src/main/java/co/edu/uniandes/csw/pasteleando/persistence/ClientePersistance.java
@@ -5,10 +5,63 @@
  */
 package co.edu.uniandes.csw.pasteleando.persistence;
 
+import co.edu.uniandes.csw.pasteleando.entities.ClienteEntity;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
 /**
  *
  * @author monicabayona
  */
 public class ClientePersistance {
+    
+    @PersistenceContext(unitName = "ClientePu")
+    protected EntityManager em;
+    
+    public ClienteEntity create( ClienteEntity entity)
+    {
+        em.persist(entity);
+        return entity;
+    }
+    
+    public ClienteEntity findByName(String name)
+    {
+        TypedQuery<ClienteEntity> query = em.createQuery("Select e From ClienteEntity Where e.name = :name", ClienteEntity.class);
+        query = query.setParameter("name", name);
+        
+        List<ClienteEntity> resultado = query.getResultList();
+        
+        if(resultado.isEmpty())
+        {
+            return null;
+        }
+        else return resultado.get(0);
+    
+    }
+    
+    public List<ClienteEntity> findAll()
+    {
+        TypedQuery query = em.createQuery("Select u From ClienteEntity u", ClienteEntity.class);
+        return query.getResultList();
+    }
+    
+    public ClienteEntity find (long id)
+    {
+        return em.find(ClienteEntity.class, id);
+        
+    }
+    
+    public ClienteEntity update(ClienteEntity entity)
+    {
+        return em.merge(entity);
+    }
+    
+    public void delete(ClienteEntity entity)
+    {
+        em.remove(entity);
+    }
+    
     
 }
