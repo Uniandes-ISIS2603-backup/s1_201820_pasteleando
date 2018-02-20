c71cfc58c2d307658c2b595db6aa5e0b58232ef5
jf.garcia <jf.garcia@ISIS2603S1-0070.sis.virtual.uniandes.edu.co>
2018-02-20 08:42:47 -0500

    Corregido test para DecoracionCatalogoPersistence.

--- pasteleando-back/src/test/java/co/edu/uniandes/csw/pasteleando/persistence/DecoracionCatalogoPersistenceTest.java
+++ pasteleando-back/src/test/java/co/edu/uniandes/csw/pasteleando/persistence/DecoracionCatalogoPersistenceTest.java
@@ -6,28 +6,16 @@
 package co.edu.uniandes.csw.pasteleando.persistence;
 
 import co.edu.uniandes.csw.pasteleando.entities.DecoracionCatalogoEntity;
-import java.util.ArrayList;
-import java.util.List;
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.transaction.HeuristicMixedException;
-import javax.transaction.HeuristicRollbackException;
-import javax.transaction.NotSupportedException;
-import javax.transaction.RollbackException;
-import javax.transaction.SystemException;
-import javax.transaction.UserTransaction;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import org.jboss.arquillian.junit.Arquillian;
-import org.junit.runner.RunWith;
 import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.core.api.annotation.Inject;
+import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import uk.co.jemos.podam.api.PodamFactory;
 import uk.co.jemos.podam.api.PodamFactoryImpl;
 
@@ -37,10 +25,23 @@ import uk.co.jemos.podam.api.PodamFactoryImpl;
  */
 @RunWith(Arquillian.class)
 public class DecoracionCatalogoPersistenceTest {
-     /**
+      /**
+     * Inyección de la dependencia a la clase DecoracionCatalogoPersistence cuyos métodos
+     * se van a probar.
+     */
+    @Inject
+    private DecoracionCatalogoPersistence decoracionCatalogoPersistence;
+    /**
+     * Contexto de Persistencia que se va a utilizar para acceder a la Base de
+     * datos por fuera de los métodos que se están probando.
+     */
+    @PersistenceContext
+    private EntityManager em;
+    
+    /**
      *
      * @return Devuelve el jar que Arquillian va a desplegar en el Glassfish
-     * embebido. El jar contiene las clases de XYZ, el descriptor de la
+     * embebido. El jar contiene las clases de Decoracion Catalogo, el descriptor de la
      * base de datos y el archivo beans.xml para resolver la inyección de
      * dependencias.
      */
@@ -52,91 +53,25 @@ public class DecoracionCatalogoPersistenceTest {
                 .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
                 .addAsManifestResource("META-INF/beans.xml", "beans.xml");
     }
-    public DecoracionCatalogoPersistenceTest() {
-    }
-    
-    /**
-     * Inyección de la dependencia a la clase DecoracionCatalogoersistence cuyos métodos
-     * se van a probar.
-     */
-    @Inject
-    private DecoracionCatalogoPersistence persistence;
 
-    /**
-     * Contexto de Persistencia que se va a utilizar para acceder a la Base de
-     * datos por fuera de los métodos que se están probando.
-     */
-    @PersistenceContext
-    private EntityManager em;
-
-    /**
-     * Variable para martcar las transacciones del em anterior cuando se
-     * crean/borran datos para las pruebas.
-     */
-    @Inject
-    UserTransaction utx;
-
-     /**
-     * Conjunto de datos para la pruebas.
-     */
-    private final List<DecoracionCatalogoEntity> data = new ArrayList<>();
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-        try {
-            utx.begin();
-            em.joinTransaction();
-            clearData();
-            insertData();
-            utx.commit();
-        } catch (IllegalStateException | SecurityException | HeuristicMixedException | HeuristicRollbackException | NotSupportedException | RollbackException | SystemException e) {
-            try {
-                utx.rollback();
-            } catch (IllegalStateException | SecurityException | SystemException e1) {
-            }
-        }
-    }
-    
-    private void clearData() {
-        em.createQuery("delete from DecoracionCatalogoEntity").executeUpdate();
-    }
-
-
-    private void insertData() {
-        PodamFactory factory = new PodamFactoryImpl();
-        for (int i = 0; i < 3; i++) {
-            DecoracionCatalogoEntity entity = factory.manufacturePojo(DecoracionCatalogoEntity.class);
-
-            em.persist(entity);
-            data.add(entity);
-        }
-    }
-    
-    @After
-    public void tearDown() {
-    }
-
-    /**
-     * Test para el método create de la clase DecoracionCatalogoPersistence.
+/**
+     * Prueba para crear una Decoracion Catalogo.
+     *
+     *
      */
     @Test
-    public void createDecoracionCatalogoEntityTest() {
-    PodamFactory factory = new PodamFactoryImpl();
-    DecoracionCatalogoEntity newEntity = factory.manufacturePojo(DecoracionCatalogoEntity.class);
-    DecoracionCatalogoEntity result = persistence.create(newEntity);
+    public void createDecoracionCatalogoTest() {
+        
+        
+        PodamFactory factory = new PodamFactoryImpl();
+        DecoracionCatalogoEntity newEntity = factory.manufacturePojo(DecoracionCatalogoEntity.class);
+        DecoracionCatalogoEntity result = decoracionCatalogoPersistence.create(newEntity);
 
-    Assert.assertNotNull(result);
-    DecoracionCatalogoEntity entity = em.find(DecoracionCatalogoEntity.class, result.getId());
-    Assert.assertNotNull(entity);
-    Assert.assertEquals(newEntity.getName(), entity.getName());
+        Assert.assertNotNull(result);
+
+        DecoracionCatalogoEntity entity = em.find(DecoracionCatalogoEntity.class, result.getId());
+
+        Assert.assertEquals(newEntity.getName(), entity.getName());
     }
-    
-}
+
+    }
