ef00247eca8bc4263e601f30bd738248de4d11a8
mashu44 <m.leona@uniandes.edu.co>
2018-02-27 09:06:45 -0500

    Cambios

--- pasteleando-back/src/test/java/co/edu/uniandes/csw/pasteleando/persistence/TarjetaPuntosPersistenceTest.java
+++ pasteleando-back/src/test/java/co/edu/uniandes/csw/pasteleando/persistence/TarjetaPuntosPersistenceTest.java
@@ -5,39 +5,44 @@
  */
 package co.edu.uniandes.csw.pasteleando.persistence;
 
-import co.edu.uniandes.csw.pasteleando.entities.FacturaEntity;
+import co.edu.uniandes.csw.pasteleando.entities.TarjetaPuntosEntity;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.junit.runner.RunWith;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import uk.co.jemos.podam.api.PodamFactory;
 import uk.co.jemos.podam.api.PodamFactoryImpl;
+
+
 
 /**
  *
  * @author m.leona
  */
 @RunWith(Arquillian.class)
-public class FacturaTest {
+
+public class TarjetaPuntosPersistenceTest {
+
     /**
      * Inyección de la dependencia a la clase EditorialPersistence cuyos métodos
      * se van a probar.
      */
     @Inject
-    private FacturaPersistence FacturaPersistence;
+    private TarjetaPuntosPersistence persistence;
+
     /**
      * Contexto de Persistencia que se va a utilizar para acceder a la Base de
      * datos por fuera de los métodos que se están probando.
      */
     @PersistenceContext
     private EntityManager em;
-    
+
     /**
      *
      * @return Devuelve el jar que Arquillian va a desplegar en el Glassfish
@@ -48,29 +53,25 @@ public class FacturaTest {
     @Deployment
     public static JavaArchive createDeployment() {
         return ShrinkWrap.create(JavaArchive.class)
-                .addPackage(FacturaEntity.class.getPackage())
-                .addPackage(DecoracionPersistence.class.getPackage())
+                .addPackage(TarjetaPuntosEntity.class.getPackage())
+                .addPackage(TarjetaPuntosPersistence.class.getPackage())
                 .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
                 .addAsManifestResource("META-INF/beans.xml", "beans.xml");
     }
 
-/**
-     * Prueba para crear un Editorial.
+    /**
+     * Prueba para crear una tarejeta de puntos.
      *
      *
      */
     @Test
-    public void createDecoracionTest() {
-        
-        
+    public void createTarjetaPuntosTest() {
         PodamFactory factory = new PodamFactoryImpl();
-        FacturaEntity newEntity = factory.manufacturePojo(FacturaEntity.class);
-        FacturaEntity result = FacturaPersistence.create(newEntity);
-
+        TarjetaPuntosEntity newEntity = factory.manufacturePojo(TarjetaPuntosEntity.class);       
+        TarjetaPuntosEntity result = persistence.create(newEntity);
         Assert.assertNotNull(result);
-
-        FacturaEntity entity = em.find(FacturaEntity.class, result.getId());
-
+        TarjetaPuntosEntity entity = em.find(TarjetaPuntosEntity.class, result.getId());
+        Assert.assertNotNull(entity);
         Assert.assertEquals(newEntity.getName(), entity.getName());
     }
 
