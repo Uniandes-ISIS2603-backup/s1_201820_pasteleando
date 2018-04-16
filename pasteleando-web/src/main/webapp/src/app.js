d7d6e958c9e033b120812bb6fb18d941c41de2fc
paolabay <mp.bayonal@uniandes.edu.co>
2018-04-16 04:59:12 -0500

    se agregaron las clases para cumplir el caso de uso crear cliente

--- pasteleando-web/src/main/webapp/src/app.js
+++ pasteleando-web/src/main/webapp/src/app.js
@@ -1,10 +1,12 @@
 (function (ng) {
     var app = angular.module('mainApp', [
         // External dependencies
-        'ui.router',
+       'ui.router',
+       'ui.bootstrap',
        
         // Internal modules dependencies       
         'pasteleandoModule',
+        'clienteModule'
         
 
     ]);
