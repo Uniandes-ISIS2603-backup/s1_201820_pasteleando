eb876fb8f9a5a3d9831b9a2bb7799cb3d05c4539
ni.ramirez10 <ni.ramirez10@uniandes.edu.co>
2018-04-19 18:26:09 -0500

    mod

---
+++ pasteleando-web/src/main/webapp/src/modules/Pqrs/pqrs.mod.js
@@ -0,0 +1,33 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+(function(ng)
+{
+     var mod = ng.module('pqrsModule', ['ui.router']);
+     
+     mod.constant("pqrsContext", "api/pqrs");
+     mod.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider)
+         {
+             var basePath = 'src/modules/pqrs/';
+             
+             $stateProvider.state('pqrsList',
+             {
+                 url:'/pqrs/list',
+                   views:
+                           {
+                               mainView:
+                             {
+                                 templateUrl:basePath+'pqrs.list.html',
+                                 controller:'pqrsCtrl',
+                                 controllerAs:'ctrl'
+                             }
+                           }
+             });
+         }]);
+     
+ })
+ (window.angular);
