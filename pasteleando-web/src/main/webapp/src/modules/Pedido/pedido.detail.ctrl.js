12c65561e25eb2f8d63099e49042e5b52dc28780
ni.ramirez10 <ni.ramirez10@uniandes.edu.co>
2018-04-23 06:41:30 -0500

    Modificacion

---
+++ pasteleando-web/src/main/webapp/src/modules/Pedido/pedido.detail.ctrl.js
@@ -0,0 +1,26 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+(function (ng) 
+{
+    var mod = ng.module("pedidoModule");
+    mod.constant("pedidoContext", "api/pedidos");
+    mod.controller('pedidoDetailCtrl', ['$scope', '$http', 'pedidoContext', '$state', '$filter',
+        
+        function ($scope, $http, pedidoContext, $state, $filter) 
+        {
+            if (($state.params.pedidoId !== undefined) && ($state.params.pedidoId !== null)) 
+            {
+               $http.get(pedidoContext + '/' + $state.params.pedidoId).then(function (response) 
+                {
+                    $scope.currentPedido = response.data; 
+                });
+            }
+        }
+    ]);
+}
+)
+(window.angular);
