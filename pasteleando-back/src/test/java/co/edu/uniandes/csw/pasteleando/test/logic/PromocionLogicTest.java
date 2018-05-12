062b058afc2c2b294a560a4504389e72b4b22f99
MIGUELHOYOS <ma.hoyosr@uniandes.edu.co>
2018-05-12 13:14:26 -0500

    a

--- pasteleando-back/src/test/java/co/edu/uniandes/csw/pasteleando/test/logic/PromocionLogicTest.java
+++ pasteleando-back/src/test/java/co/edu/uniandes/csw/pasteleando/test/logic/PromocionLogicTest.java
@@ -7,8 +7,6 @@ import co.edu.uniandes.csw.pasteleando.exceptions.BusinessLogicException;
 import co.edu.uniandes.csw.pasteleando.persistence.PromocionPersistence;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
