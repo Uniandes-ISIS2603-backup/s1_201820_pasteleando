16ab47c223556d8d008edc0c1873382df6abd556
jf.garcia <jf.garcia@ISIS2603S1-0070.sis.virtual.uniandes.edu.co>
2018-05-16 14:25:41 -0500

    En el index cambio una "c" por mayúscula y en DecoDTO borro un duplicado.

--- pasteleando-web/src/main/java/co/edu/uniandes/csw/pasteleando/dtos/DecoracionDTO.java
+++ pasteleando-web/src/main/java/co/edu/uniandes/csw/pasteleando/dtos/DecoracionDTO.java
@@ -52,10 +52,6 @@ public class DecoracionDTO implements Serializable {
      * Atributo que modela si la decoracion es Personalizada
      */
     private Integer esPersonalizada = 0;
-   /**
-     * Atributo que modela la foto de la decoracion Personalizada
-     */
-    private String foto;
 
     private Long id;
 
